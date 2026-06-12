0d7087e703e5b85b2937931096516b1fbf534015
jinhoDevOps <okho04@gmail.com>
2026-06-13 02:27:05 +0900

    fix(ui): unify component curvatures (border-radii) and button sizes to enforce Flat Editorial design principles

--- shared/core/utils.js
+++ shared/core/utils.js
@@ -175,7 +175,7 @@ export const IsfUtils = (function initIsfUtils(global) {
   }
 
   const result = {
-    APP_VERSION: (typeof __APP_VERSION__ !== "undefined") ? __APP_VERSION__ : "0.11.36",
+    APP_VERSION: (typeof __APP_VERSION__ !== "undefined") ? __APP_VERSION__ : "0.11.37",
     formatMoney,
     getFinancialIncomeStatus,
     calculateIncomeTax,
